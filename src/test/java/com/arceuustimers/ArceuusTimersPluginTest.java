89b2146e7a3cfde4e46c666ded905bd834337165
Mantic <55759474+Mantic-says-hi@users.noreply.github.com>
2023-03-22 17:06:12 +0800

    Rename / Refactor files and fields from example plugin

--- src/test/java/com/arceuustimers/ArceuusTimersPluginTest.java
+++ src/test/java/com/arceuustimers/ArceuusTimersPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.arceuustimers;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ArceuusTimersPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ArceuusTimersPlugin.class);
 		RuneLite.main(args);
 	}
 }
