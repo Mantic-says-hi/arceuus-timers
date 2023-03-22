b4f0224e11611694666b818381d6f74140f0cee6
Mantic <55759474+Mantic-says-hi@users.noreply.github.com>
2023-03-22 17:05:42 +0800

    Rename / Refactor files and fields from example plugin

--- src/main/java/com/arceuustimers/ArceuusTimersConfig.java
+++ src/main/java/com/arceuustimers/ArceuusTimersConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.arceuustimers;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface ArceuusTimersConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
