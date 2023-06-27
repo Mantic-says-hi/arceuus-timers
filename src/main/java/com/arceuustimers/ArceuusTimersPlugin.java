6734f484bc58c213163582d2705729f2b972b5a1
Mantic <55759474+Mantic-says-hi@users.noreply.github.com>
2023-06-28 02:52:11 +0800

    - README.md more clear - Add text format option

--- src/main/java/com/arceuustimers/ArceuusTimersPlugin.java
+++ src/main/java/com/arceuustimers/ArceuusTimersPlugin.java
@@ -5,14 +5,12 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.Varbits;
+import net.runelite.api.*;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.events.VarbitChanged;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
