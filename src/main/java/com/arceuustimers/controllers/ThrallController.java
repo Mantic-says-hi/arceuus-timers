e897f2c231dded5b7bf72d1eab1ea1ba22a7394b
Mantic <55759474+Mantic-says-hi@users.noreply.github.com>
2025-06-25 23:36:24 +0800

    2.0.0 version - Summer Sweep Up

--- src/main/java/com/arceuustimers/controllers/ThrallController.java
+++ src/main/java/com/arceuustimers/controllers/ThrallController.java
@@ -4,7 +4,6 @@ import com.arceuustimers.ArceuusTimersPlugin;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.Skill;
-import net.runelite.api.Varbits;
 import net.runelite.api.gameval.VarbitID;
 import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
 @Slf4j
@@ -13,8 +12,7 @@ public class ThrallController extends SpellController
 	private final Client client;
 	private static final double GAME_TICK = 0.6;
 	private static final int UNLOCKED = 2;
-	private static final double GM_MULTIPLIER = 2.0;
-	private static final double M_MULTIPLIER = 1.5;
+	private static final double THRALL_MULTIPLIER = 2.0;
 
 	public ThrallController(String fileName, double cooldown, String tooltip, InfoBoxManager manager, ArceuusTimersPlugin plugin, Client client)
 	{
@@ -26,13 +24,10 @@ public class ThrallController extends SpellController
 	{
 		if (getActive()) shutdown();
 		double thrallUptime = GAME_TICK * client.getBoostedSkillLevel(Skill.MAGIC);
-		if (client.getVarbitValue(VarbitID.CA_TIER_STATUS_GRANDMASTER ) == UNLOCKED)
+		if ((client.getVarbitValue(VarbitID.CA_TIER_STATUS_GRANDMASTER ) == UNLOCKED) ||
+		    (client.getVarbitValue(VarbitID.CA_TIER_STATUS_MASTER) == UNLOCKED))
 		{
-			thrallUptime *= GM_MULTIPLIER;
-		}
-		else if (client.getVarbitValue(VarbitID.CA_TIER_STATUS_MASTER) == UNLOCKED)
-		{
-			thrallUptime *= M_MULTIPLIER;
+			thrallUptime *= THRALL_MULTIPLIER;
 		}
 		thrallUptime -= GAME_TICK; // 1-tick delay for box creation
 		setCooldown(thrallUptime);
